8e853d22db0ba7a716b86875bb2cc67c4244e870
Atharv_Kondekar <atharvkondekar96@gmail.com>
2025-04-15 18:00:20 -0700

    Add files via upload

---
+++ Static Variables/Static_data_members02.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class item{
+	
+	private:
+		static int count;
+		int Data;
+	
+	public:
+		void GetData(int a){
+			Data=a;
+			count++;
+		}
+		
+		void DisplayCount()
+		{
+			cout<<"\n count : "<<count;
+		}
+};
+
+int item::count=10;
+
+int main()
+{
+	item a,b,c;
+	a.DisplayCount();
+	b.DisplayCount();
+	c.DisplayCount();
+	
+	a.GetData(100);
+	a.GetData(200);
+	c.GetData(300);
+	
+	cout<<"\n\n After Giving the Data ";
+	
+	a.GetData(100);
+	a.DisplayCount();
+	
+	a.GetData(200);
+	b.DisplayCount();
+	
+	c.GetData(300);
+	c.DisplayCount();
+	
+	cout<<"\n\n Now Displaying the GetCount() Funtion of all object ";
+	
+	a.DisplayCount();
+	b.DisplayCount();
+	c.DisplayCount();
+	
+	return 0;
+}
