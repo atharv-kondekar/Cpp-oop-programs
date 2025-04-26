da69f87effa2dd0216d9fe650d0d713c1a2c702e
Atharv_Kondekar <atharvkondekar96@gmail.com>
2025-04-25 19:03:43 -0700

    Add files via upload

---
+++ Constructors & Destructors/Destructors/destructor_for_Dyanamically_Memory02.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Simple{
+	private:
+		int *data;
+	
+	public:
+		Simple();
+		void Set();
+		void Display();
+		~Simple();
+};
+
+Simple::Simple()
+{
+	data=new int;
+	*data=0;
+}
+
+void Simple::Set()
+{
+	cout<<"\n Enetr the data : ";
+	cin>>*data;
+}
+
+void Simple::Display()
+{
+	cout<<"\n The Data : "<<*data;
+}
+
+Simple::~Simple()
+{
+	delete data;
+	cout<<"\n\n The Memory is Released Successfully";
+}
+
+int main()
+{
+
+	{
+		Simple s1;
+		s1.Set();
+		s1.Display();
+	}
+	
+	Simple s2;
+	s2.Display();
+	
+	// The Destructor is called by constructor automatically 
+	// It not called by Explicitly
+	
+	
+	return 0;
+}
